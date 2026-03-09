f60c22936f0d35466dde37fb43d8b540859b3485
jackson-91-henrique <jackson91henrique@gmail.com>
2026-03-09 16:51:24 -0300

    Update app.run to use environment PORT variable

--- main.py
+++ main.py
@@ -312,4 +312,5 @@ def send_email(name, email, phone, message):
         connection.sendmail(MAIL_ADDRESS, MAIL_ADDRESS, email_message)
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
